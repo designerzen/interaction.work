cdba845461faab11c82e021488c8bedfc42a8fcb
zen <designerzen@gmail.com>
2026-01-24 19:05:44 +0000

    feat: vite and vittest

--- apps/photosynth/vite.config.ts
+++ apps/photosynth/vite.config.ts
@@ -1,12 +1,31 @@
 import { defineConfig } from "vite"
+import { viteConvertPugInHtml } from '@mish.dev/vite-convert-pug-in-html'
+import { resolve } from 'path'
 
 export default defineConfig({
-    clearScreen: false,
-    server: {
-        port: 1420,
-        strictPort: true,
-        watch: {
-            ignored: ["**/src-tauri/**"],
-        },
-    },
+	clearScreen: false,
+	plugins: [
+		viteConvertPugInHtml({
+			locals: {
+				SITE_NAME: "Photosynth",
+				CURRENT_YEAR: new Date().getFullYear(),
+			}
+		})
+	],
+	root: "source",
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'source'),
+		},
+	},
+	build: {
+		outDir: "../dist/"
+	},
+	server: {
+		port: 909,
+		strictPort: true,
+		watch: {
+			ignored: ["**/source-tauri/**"]
+		}
+	}
 })
