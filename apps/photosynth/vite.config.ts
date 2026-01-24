1372e86e1ddfe70f3e98865a1fc5793cb6344e61
zen <designerzen@gmail.com>
2026-01-24 23:09:44 +0000

    feat: server setup

--- apps/photosynth/vite.config.ts
+++ apps/photosynth/vite.config.ts
@@ -1,18 +1,30 @@
 import { defineConfig } from "vite"
 import { viteConvertPugInHtml } from '@mish.dev/vite-convert-pug-in-html'
 import { resolve } from 'path'
+import packageJSON from './package.json'
+import markdownPlugin from './vite-plugin-markdown'
 
+const instruments:string[] = []
+
+// We can pass whatever JS vars we want into the template
+// by adding it here to locals
+// we mainly use this for dates and versions
+// but it is also handy for connecting together other data
 export default defineConfig({
 	clearScreen: false,
 	plugins: [
+		markdownPlugin(),
 		viteConvertPugInHtml({
 			locals: {
-				SITE_NAME: "Photosynth",
 				CURRENT_YEAR: new Date().getFullYear(),
+				package:packageJSON,
+				instrumentNames:instruments,
+				instruments:instruments.map( instrument => `<li>${instrument}</li>` ).join()
 			}
 		})
 	],
 	root: "source",
+	publicDir: "../public",
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'source'),
